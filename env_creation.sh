09cbf7c4a62c60908b07b202a4780f8ca5542f23
viswanath27 <code.vissu@gmail.com>
2022-09-25 15:13:26 +0530

    updated the env creation script
    updated the env creation script

--- env_creation.sh
+++ env_creation.sh
@@ -1,3 +1,5 @@
+rm -rf ~/webscraping
+git clone https://github.com/viswanath27/webscraping.git
 echo "Setting the servers"
 sudo apt install software-properties-common --assume-yes
 sudo add-apt-repository ppa:deadsnakes/ppa -y
@@ -15,9 +17,22 @@ echo "Acticate virtual env"
 source ~/rasa_env/bin/activate
 echo "Installing Rasa with Transformers"
 pip install rasa[transformers] --quiet
+pip3 install boto3 --quiet
+pip3 install flask
+export AWS_SECRET_ACCESS_KEY=3oTbM1F+BENxWrhHFRub7gNfB2DtvfhvkztpdSLV
+export AWS_ACCESS_KEY_ID=AKIAZ74VWP2NKSAYXVTV
+export AWS_DEFAULT_REGION=ap-northeast-1
+export BUCKET_NAME=ssrasafinancebot
+export AWS_ENDPOINT_URL=https://ssrasafinancebot.s3.ap-northeast-1.amazonaws.com/models/20220903-172133-khaki-lion.tar.gz
+
 echo "create rasa core dir"
-cd webscraping/src/rasa
 #mkdir rasa_core
 #cd rasa_core
-echo "Start rasa_init"
-rasa init --no-prompt
+cd webscraping/src/rasa
+echo "Start rasa core .."
+rasa run --model 20220903-172133-khaki-lion.tar.gz --remote-storage aws &
+echo "Start rasa action server .."
+rasa run actions &
+echo "Starting the UI server .."
+cd ~/webscraping/src/ui_app
+python3 app.py &
